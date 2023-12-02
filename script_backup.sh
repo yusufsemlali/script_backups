2c7f016f50a42d684d6bc737f063c1bfbb489e1f
YSFsemlali <ysfm.102@gmail.com>
2023-12-02 23:38:55 +0100

    Backup scripts and .zshrc

--- script_backup.sh
+++ script_backup.sh
@@ -13,11 +13,6 @@ cp ~/.zshrc $SCRIPT_DIR
 # Navigate to the script directory
 cd $SCRIPT_DIR
 
-# Initialize a new Git repository (if one doesn't already exist)
-if [ ! -d .git ]; then
-    git init
-fi
-
 # Add the files to the repository
 git add .
 
@@ -25,5 +20,4 @@ git add .
 git commit -m "Backup scripts and .zshrc"
 
 # Push the changes to GitHub
-# Replace 'origin' and 'master' with your remote name and branch if they're different
 git push origin master
